e5b58853125d18c71c051b817dce4191ef201998
liyas1719 <lasariaissa@smith.edu>
2024-10-02 20:47:27 -0400

    changed

--- Passenger.java
+++ Passenger.java
@@ -1,8 +1,19 @@
 public class Passenger {
     
-    private String name;
+    public String name;
 
     public Passenger(String name) {
         this.name = name;
+        
+   // public void boardcar(Car c) {
+
+    //}
+
     }
 }
+// Now that you've got a functional `Car` class, the `Passenger` class can be expanded to use the `Car`'s methods to implement some of its own:
+
+// - `boardCar(Car c)` can call `c.addPassenger(this)` to board a given `Car` (_Hint: this method should check the value that gets `return`ed 
+// by `c.addPassenger(...)` in case the selected car is full._)
+// - `getOffCar(Car c)` can call `c.removePassenger(this)` to get off a given `Car` (_Hint: this method should check the value that gets 
+// `return`ed by `c.removePassenger(...)` in case the `Passenger` wasn't actually onboard._)
